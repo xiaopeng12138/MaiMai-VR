57fe5b4a8cc4f83e867f22234d3928b225f422ac
Thalesalex <thalesperes@yahoo.com.br>
2021-12-13 03:30:00 -0300

    Improve Touch detection

--- Assets/RegisterTouchSerial.cs
+++ Assets/RegisterTouchSerial.cs
@@ -1,34 +1,36 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using System.Collections;
 using UnityEngine;
 
-    public class RegisterTouchSerial : MonoBehaviour
+public class RegisterTouchSerial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        public int Area;
-
-        private void OnTriggerEnter(Collider other)
-        {
-            //Debug.Log("trigger Touch");
-            Serial.ChangeTouch((int)Area, true);
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            Serial.ChangeTouch((int)Area, false);
-            Serial.ChangeTouch((int)Area, false);
-        }
 
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int Area;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("trigger Touch");
+        Serial.ChangeTouch((int)Area, true);
+        Serial.SendTouch();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Serial.ChangeTouch((int)Area, false);
+        Serial.ResetTouch();
+
+    }
+
+}
